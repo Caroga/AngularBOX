afc28ce6a1ee8074ea3fb1623b78f099de33b33f
Roberto Gardenier <roberto@caroga.net>
2014-11-16 15:24:23 +0100

    Added grunt-contrib-concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,8 @@
 module.exports = function (grunt) {
     grunt.initConfig({
+        //Read package information
+        pkg: grunt.file.readJSON('package.json'),
+
         bower: {
             install: {
                 options: {
@@ -11,9 +14,24 @@ module.exports = function (grunt) {
                     bowerOptions: {}
                 }
             }
+        },
+
+        concat: {
+            options: {
+                separator: ';',
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                src: [
+                    'vendor/**/*.js',
+                    'app/*.js'
+                ],
+                dest: 'dist/js/<%= pkg.name %>-<%= pkg.version %>.js'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 };
 
